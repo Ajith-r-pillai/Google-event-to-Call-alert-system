4e55cb05ec21b71ea82463c7d66a14c51853b4b1
Ajith-r-pillai <ajithkumarr1108@gmail.com>
2026-01-17 09:44:28 +0530

    first commit

--- src/lib/googleCalendar.ts
+++ src/lib/googleCalendar.ts
@@ -1,20 +1,20 @@
 export async function getUpcomingEvents(accessToken: string) {
   const now = new Date();
 
-const fifteenMinLater = new Date(now.getTime() + 15 * 60 * 1000);
+  const fiveMinLater = new Date(now.getTime() + 5 * 60 * 1000);
 
 
- const res = await fetch(
-  `https://www.googleapis.com/calendar/v3/calendars/primary/events?` +
-    `timeMin=${now.toISOString()}` +
-    `&timeMax=${fifteenMinLater.toISOString()}` +
-    `&singleEvents=true&orderBy=startTime`,
-  {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  }
-);
+  const res = await fetch(
+    `https://www.googleapis.com/calendar/v3/calendars/primary/events?` +
+      `timeMin=${now.toISOString()}` +
+      `&timeMax=${fiveMinLater.toISOString()}` +
+      `&singleEvents=true&orderBy=startTime`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
 
 
   const data = await res.json();
